ae869d7103799bfa6263ce1a687eb01a50cf09b3
darklord777 <bulk@nulvoid.net>
2016-06-06 17:46:07 +0000

    Fixing missing semicolon

--- 3-jade.js
+++ 3-jade.js
@@ -6,7 +6,7 @@ app.set('view engine', 'jade');
 app.set('views', process.argv[3]||path.join(__dirname, 'templates'));
 
 app.get( '/home', function(req, res, next) {
-    res.render('index', {date: new Date().toDateString()}) 
+    res.render('index', {date: new Date().toDateString()});
 });
 
 app.listen( process.argv[2] );
