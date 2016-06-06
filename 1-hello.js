ed298b75385dfd30124ad3234aa8b1846e6f6397
darklord777 <bulk@nulvoid.net>
2016-06-06 14:55:05 +0000

    Completed exercise 1 - 'hello world'

---
+++ 1-hello.js
@@ -0,0 +1,10 @@
+var http = require("http");
+var express = require("express");
+
+var app = express();
+
+app.get('/home', function(req,res) {
+    res.end('Hello World!');
+})
+
+app.listen(process.argv[2]);
